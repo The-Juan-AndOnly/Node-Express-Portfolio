d6dc0744fbd4f16cf2d575b0c0771aba9b8bd57b
Juan Lopez <juanl0215@gmail.com>
2019-06-15 10:23:11 -0400

    Setup a routes directory

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
+const route = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 // app.use(express.static('public'));
 // Setup Pug
 app.set('view engine', 'pug');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 // Listen for the port that the server is running on and display a message in console
 app.listen(PORT, () => {
