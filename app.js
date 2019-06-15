03460fe3c286da1c970eac45fdb66a4c732c073a
Juan Lopez <juanl0215@gmail.com>
2019-06-15 10:01:05 -0400

    Setup basic app.js structure

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Working');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('<h5>Hello Douchebag</h5>');
+});
+
+// Listen for the port that the server is running on and display a message in console
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
