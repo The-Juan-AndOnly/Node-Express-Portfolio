5d7982be2024b303b4400d825a5a284d6d497f81
Juan Lopez <juanl0215@gmail.com>
2019-06-18 07:27:01 -0400

    Added eror handling and error route

--- app.js
+++ app.js
@@ -6,13 +6,28 @@ const PORT = process.env.PORT || 3000;
 // Setup Pug
 app.set('view engine', 'pug');
 
+// import Routes
 const mainRoutes = require('./routes');
 const projectRoutes = require('./routes/projects');
+
 // Create static Route for public folder which contains CSS and JS
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 app.use(mainRoutes);
 app.use('/projects', projectRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error', err);
+});
 
 // Listen for the port that the server is running on and display a message in console
 app.listen(PORT, () => {
--- routes/projects.js
+++ routes/projects.js
@@ -4,13 +4,7 @@ const { data } = require('../data/data.json');
 const { projects } = data;
 
 router.get('/', (req, res) => {
-  res.render('project', {
-    title: projects[0].project_name,
-    description: projects[0].description,
-    technologies: projects[0].technologies,
-    live_link: projects[0].live_link,
-    github_link: projects[0].github_link
-  });
+  res.render('project', { projects });
 });
 
 module.exports = router;
